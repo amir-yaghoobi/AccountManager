acd6797f94dce343a2fcd550338c47223c2e0c58
Amirhossein Yaghoobi <a.yaghoobi.dev@gmail.com>
2018-09-20 23:07:16 +0430

    reformat code

--- server/routes.go
+++ server/routes.go
@@ -39,32 +39,32 @@ func getApiRoutes() (router *gin.Engine) {
 	router.GET("/", apiStatusController)
 
 	authMiddleWare := middlewares.SetupJWT()
-	router.POST("/auth",                             authMiddleWare.LoginHandler)
+	router.POST("/auth",                                 authMiddleWare.LoginHandler)
 
 	userGroup := router.Group("/user")
-	userGroup.POST("/register",                       controllers.Register)
+	userGroup.POST("/register",                          controllers.Register)
 	{
 		userGroup.Use(authMiddleWare.MiddlewareFunc())
-		userGroup.GET("/:userId",                     controllers.GetUser)
+		userGroup.GET("/:userId",                        controllers.GetUser)
 	}
 
 	accountGroup := router.Group("/account")
 	{
 		accountGroup.Use(authMiddleWare.MiddlewareFunc())
-		accountGroup.GET("/",                         controllers.NotImplementedYet)
-		accountGroup.POST("/",                        controllers.CreateNewAccount )
-		accountGroup.POST("/inviteUser",              controllers.NotImplementedYet)
-		accountGroup.DELETE("/:accountId",            controllers.NotImplementedYet)
+		accountGroup.GET("/",                            controllers.NotImplementedYet)
+		accountGroup.POST("/",                           controllers.CreateNewAccount )
+		accountGroup.POST("/inviteUser",                 controllers.NotImplementedYet)
+		accountGroup.DELETE("/:accountId",               controllers.NotImplementedYet)
 	}
 
 	incomeGroup := router.Group("/income")
 	{
 		incomeGroup.Use(authMiddleWare.MiddlewareFunc())
-		incomeGroup.POST("/",                            controllers.AddIncome        )
-		incomeGroup.GET("/:accountId",                   controllers.NotImplementedYet)
-		incomeGroup.GET("/:accountId/category",          controllers.GetAccountCategories)
-		incomeGroup.GET("/:accountId/category/:cId",     controllers.GetAccountCategories)
-		incomeGroup.POST("/:accountId/category",         controllers.AddCategory      )
+		incomeGroup.POST("/",                             controllers.AddIncome        )
+		incomeGroup.GET("/:accountId",                    controllers.NotImplementedYet)
+		incomeGroup.GET("/:accountId/category",           controllers.GetAccountCategories)
+		incomeGroup.GET("/:accountId/category/:cId",      controllers.GetAccountCategories)
+		incomeGroup.POST("/:accountId/category",          controllers.AddCategory      )
 	}
 
 	expenseGroup := router.Group("/expense")
@@ -80,10 +80,10 @@ func getApiRoutes() (router *gin.Engine) {
 	budgetGroup := router.Group("/budget")
 	{
 		budgetGroup.Use(authMiddleWare.MiddlewareFunc())
-		budgetGroup.POST("/",                          controllers.NotImplementedYet)
-		budgetGroup.GET("/:accountId",                 controllers.NotImplementedYet)
-		budgetGroup.PUT("/:accountId/:budgetId",       controllers.NotImplementedYet)
-		budgetGroup.DELETE("/:accountId/:budgetId",    controllers.NotImplementedYet)
+		budgetGroup.POST("/",                             controllers.NotImplementedYet)
+		budgetGroup.GET("/:accountId",                    controllers.NotImplementedYet)
+		budgetGroup.PUT("/:accountId/:budgetId",          controllers.NotImplementedYet)
+		budgetGroup.DELETE("/:accountId/:budgetId",       controllers.NotImplementedYet)
 	}
 
 	return router
