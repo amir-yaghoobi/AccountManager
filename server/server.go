3525c3a7a6fd5ab4a825c7a23ee18d832ed9ba83
Amirhossein Yaghoobi <a.yaghoobi.dev@gmail.com>
2018-09-27 18:48:41 +0330

    cleanup unused functions

--- server/server.go
+++ server/server.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
 	"os"
-	"github.com/amir-yaghoobi/accountManager/db"
-	"time"
-	"github.com/amir-yaghoobi/accountManager/models"
-	"github.com/jinzhu/gorm"
-	"github.com/amir-yaghoobi/accountManager/config"
 	"fmt"
+	"time"
+	log "github.com/sirupsen/logrus"
+	"github.com/amir-yaghoobi/accountManager/db"
+	"github.com/amir-yaghoobi/accountManager/config"
 )
 
 var serverStartedAt time.Time
@@ -19,73 +17,6 @@ func initLogger() {
 	log.SetLevel(log.DebugLevel)
 }
 
-func createSampleUserAndAccounts(conn *gorm.DB) {
-	//admin := models.User{
-	//	FirstName: "امیرحسین",
-	//	LastName: "یعقوبی",
-	//	Username: "amir",
-	//	Password: "battle2021",
-	//	Avatar: "awesome.jpg",
-	//	LastIp: "127.0.0.1",
-	//	LastAccess: time.Now(),
-	//}
-	//
-	//conn.Save(&admin)
-	//
-	//amir := models.User{
-	//	FirstName: "امیرحسین",
-	//	LastName: "یعقوبی",
-	//	Username: "amir",
-	//	Password: "battle2021",
-	//	Avatar: "awesome.jpg",
-	//	LastIp: "127.0.0.1",
-	//	LastAccess: time.Now(),
-	//}
-	//
-	//conn.Save(&amir)
-	//
-	acc := models.Account{
-		Name: "خانه",
-		Icon: "family.png",
-	}
-	conn.Save(&acc)
-
-	var user models.User
-	conn.First(&user, 1)
-	//conn.Model(&acc).Association("Users").Append([]models.User{amir, admin})
-	conn.Model(&acc).Association("Users").Append([]models.User{user})
-}
-
-func loadAccountsWithUsers(conn *gorm.DB) {
-	var account models.Account
-	conn.Preload("Users").First(&account)
-
-	log.Info(account)
-	for _, user := range account.Users {
-		log.Info(user)
-	}
-}
-
-func createCategoriesAndSubCategories(conn *gorm.DB) {
-	var account models.Account
-	conn.First(&account)
-
-	cat := models.IncomeCategory{
-		Account: account,
-		Name: "H@$@",
-		Icon: "SAD.ico",
-	}
-
-	conn.Save(&cat)
-
-	subCats := []models.IncomeCategory{
-		{Name: "hd1", Icon: "hd1", Account: account},
-		{Name: "hd2", Icon: "hd2", Account: account},
-		{Name: "hd3", Icon: "hd3", Account: account},
-	}
-
-	conn.Model(&cat).Association("SubCategories").Append(subCats)
-}
 
 func main() {
 	initLogger()
